3d75e661da042e2547948a5079e0ed1e70c2ce90
hminh35789 <hminh35789@gmail.com>
2024-01-24 22:53:11 +0700

    update cart

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,26 +1,16 @@
 import Link from 'next/link'
 import React from 'react'
+import Menu from './Menu'
 
 const Header = () => {
   return (
     <header>
       <nav>
-        <div className='navbar justify-between bg-base-300'>
+        <div className='navbar justify-between bg-base-300  '>
           <Link href='/' className='btn btn-ghost text-lg'>
             Ikiguy
           </Link>
-          <ul className='flex'>
-            <li>
-              <Link href='/cart' className='btn btn-ghost rounded-btn'>
-                Cart
-              </Link>
-            </li>
-            <li>
-              <Link href='/signin' className='btn btn-ghost rounded-btn'>
-                Sign in
-              </Link>
-            </li>
-          </ul>
+          <Menu />
         </div>
       </nav>
     </header>
